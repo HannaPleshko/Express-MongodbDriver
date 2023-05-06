3c53e4030bf5595c0a8338a49e541e2b3c465638
Hanna <hannapleshko2000@mail.ru>
2023-05-06 09:22:30 +0300

    linter

--- src/app.js
+++ src/app.js
@@ -8,19 +8,19 @@ const user = require('./controller/user.controller');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/user', user);
 
-app.use((error, req, res, next) => {
-    const status = error.status || 500;
-    const message = error.message || 'Something went wrong';
-    const id = error.id || 0;
+app.use((error, req, res, _next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong';
+  const id = error.id || 0;
 
-    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}, ID:: ${id}`);
-    res.status(status).send({ id, message });
+  logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}, ID:: ${id}`);
+  res.status(status).send({ id, message });
 });
 
 module.exports = app;
